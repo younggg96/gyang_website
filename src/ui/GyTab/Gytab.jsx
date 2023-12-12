c896e8d1d8e61691e60cd652ddfb16e9a7d1779f
Guanggenng Yang <yangguanggeng960123@gmail.com>
2023-12-11 18:58:25 -0800

    update tab ui

--- src/ui/GyTab/Gytab.jsx
+++ src/ui/GyTab/Gytab.jsx
@@ -3,6 +3,7 @@ import "./index.scss";
 import classNames from "classnames";
 
 const Gytab = ({ data, activeIndex, setActiveIndex, mobile, children, className,...props }) => {
+  
   return (
     <>
       <ul className={classNames([{ "gy-tab": !mobile, "gy-tab-mobile": mobile }, className])} {...props}>
@@ -12,12 +13,14 @@ const Gytab = ({ data, activeIndex, setActiveIndex, mobile, children, className,
               title={item}
               className={classNames({ title: activeIndex === index })}
               key={index}
+              role="tab"
               onClick={() => setActiveIndex(index)}
             >
               {item}
             </li>
           );
         })}
+        <div className="underline" style={{ transform: `translateX(${activeIndex * 120}px)`}}></div>
       </ul>
       {children}
     </>
